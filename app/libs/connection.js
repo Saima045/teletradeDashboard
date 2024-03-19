8aef82262c473a69f16a634632fbd2781051e9f7
saima <saima045@gmail.com>
2024-03-20 00:36:49 +0500

    “ft”

--- app/libs/connection.js
+++ app/libs/connection.js
@@ -3,6 +3,25 @@ let cachedClient = null;
 
 export const connectDB = async () => {
 
+
+   
+    if (cachedClient && cachedClient.readyState === 1) {
+        return cachedClient;
+    }
+
+    try {
+        const client = await mongoose.connect('mongodb+srv://saima045:amnanasir@martdb.fisgkoz.mongodb.net/martdb?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        cachedClient = client;
+        return client;
+    } catch (error) {
+        throw new Error('Failed to connect to MongoDB Atlas: ' + error.message);
+    }
+
+
     // if (cachedClient && cachedClient.isConnected()) {
     //     return cachedClient;
     //   }
@@ -12,14 +31,14 @@ export const connectDB = async () => {
     //   return client;
 
     
-    const connected = {};
-    if (connected.isConnected) return;
-    try {
-        const db = await mongoose.connect('mongodb+srv://saima045:amnanasir@martdb.fisgkoz.mongodb.net/martdb?retryWrites=true&w=majority')
-        connected.isConnected = db.connections[0].readyState;
-    } catch (error) {
-        console.log(error)
-    }
-    return connected
+    // const connected = {};
+    // if (connected.isConnected) return;
+    // try {
+    //     const db = await mongoose.connect('mongodb+srv://saima045:amnanasir@martdb.fisgkoz.mongodb.net/martdb?retryWrites=true&w=majority')
+    //     connected.isConnected = db.connections[0].readyState;
+    // } catch (error) {
+    //     console.log(error)
+    // }
+    // return connected
 
 }
