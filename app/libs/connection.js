fe839ea8b5f30b55b5f6bdd26a452a20d6f2d608
saima <saima045@gmail.com>
2024-03-20 00:05:25 +0500

    “first1”

--- app/libs/connection.js
+++ app/libs/connection.js
@@ -3,22 +3,23 @@ let cachedClient = null;
 
 export const connectDB = async () => {
 
-    if (cachedClient && cachedClient.isConnected()) {
-        return cachedClient;
-      }
+    // if (cachedClient && cachedClient.isConnected()) {
+    //     return cachedClient;
+    //   }
     
-      const client = await mongoose.connect('mongodb+srv://saima045:amnanasir@martdb.fisgkoz.mongodb.net/martdb?retryWrites=true&w=majority')
-      cachedClient = client;
-      return client;
+    //   const client = await mongoose.connect('mongodb+srv://saima045:amnanasir@martdb.fisgkoz.mongodb.net/martdb?retryWrites=true&w=majority')
+    //   cachedClient = client;
+    //   return client;
 
     
-    // const connected = {};
-    // if (connected.isConnected) return;
-    // try {
-    //     const db = await mongoose.connect('mongodb+srv://saima045:amnanasir@martdb.fisgkoz.mongodb.net/martdb?retryWrites=true&w=majority')
-    //     connected.isConnected = db.connections[0].readyState;
-    // } catch (error) {
-    //     console.log(error)
-    // }
+    const connected = {};
+    if (connected.isConnected) return;
+    try {
+        const db = await mongoose.connect('mongodb+srv://saima045:amnanasir@martdb.fisgkoz.mongodb.net/martdb?retryWrites=true&w=majority')
+        connected.isConnected = db.connections[0].readyState;
+    } catch (error) {
+        console.log(error)
+    }
+    return connected
 
 }
